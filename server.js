c8629b9f13b05877f7ee4b55d7742def700759cf
James Pasko <jbpasko@gmail.com>
2014-01-10 11:33:38 -0800

    listen for worker exit event rather than death (which isn't in docs)

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ if (cluster.isMaster) {
     for (var i = 0; i < count; i++) {
 	spawn();
     }
-    cluster.on("death", function(worker) {
+    cluster.on("exit", function(worker) {
 	console.log("worker " + worker.pid + " died. spawning a new process.");
 	delete workers[worker.pid];
 	spawn();
